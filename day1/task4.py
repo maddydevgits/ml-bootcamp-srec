adfd794267e6a2b92b57833e10613dc5401cce52
MS <72651078+madblocksgit@users.noreply.github.com>
2021-11-26 11:44:49 +0530

    Create task4.py

---
+++ day1/task4.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_ngrok import run_with_ngrok
+
+app=Flask(__name__)
+run_with_ngrok(app)
+
+@app.route('/')
+def printName():
+  return ("Maddy")
+if __name__=="__main__":
+  app.run()
