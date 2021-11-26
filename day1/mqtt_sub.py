924b2d655c7dcc2db3dd97be5c84b1daead3b71c
MS <72651078+madblocksgit@users.noreply.github.com>
2021-11-26 11:47:11 +0530

    Create mqtt_sub.py

---
+++ day1/mqtt_sub.py
@@ -0,0 +1,13 @@
+import paho.mqtt.client as mqtt
+
+client=mqtt.Client()
+client.connect('broker.hivemq.com',1883)
+print ('Broker Connected')
+
+client.subscribe('srec/ml')
+
+def notification(client,userdata,msg):
+  print (msg.payload)
+
+client.on_message=notification
+client.loop_forever()
